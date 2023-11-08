522f616408fc76301939d7ed689d0366bf219d53
cataldosara98 <sara.cataldo00@gmail.com>
2023-11-08 14:34:20 +0100

    Prima push di Onelia

--- Progetto_SAD.R
+++ Progetto_SAD.R
@@ -16,3 +16,5 @@ deaths_2010 <- aggregate(Value ~ Country, data = subset_2010, FUN = sum)
 deaths_ordered <- deaths_2010[order(deaths_2010$Value),]
 
 barplot(deaths_ordered$Value,names.arg = deaths_ordered$Country, las=2, col=rainbow(length(deaths_ordered$Value)),cex.names = 0.5,main="Barplot Deaths in 2010")
+
+summary(data_infant) 
