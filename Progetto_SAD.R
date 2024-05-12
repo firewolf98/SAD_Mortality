9bad160eb2302930a8ac3796f62ce0a3a4237b29
cataldosara98 <sara.cataldo00@gmail.com>
2024-05-12 16:24:47 +0200

    Ultima modifica

--- Progetto_SAD.R
+++ Progetto_SAD.R
@@ -1,5 +1,4 @@
 library(dplyr)
-library(readxl)
 
 data <- read_xlsx("Mortality.xlsx")
 names(data)[names(data)=="Variable"] <- "Kind of death"
@@ -13,7 +12,7 @@ data_infant$Variable <- as.factor(data_infant$Variable)
 data_infant$Measure <- as.factor(data_infant$Measure)
 data_infant$Country <- as.factor(data_infant$Country)
 data_infant$Year <- as.factor(data_infant$Year)
-#Ho eliminato la colonna Flags in quanto non è rilevante ai fini dell'analisi statistica
+#Sara- Ho eliminato la colonna Flags in quanto non è rilevante ai fini dell'analisi statistica
 data_infant$Flags <- NULL
 
 summary(data_infant)
@@ -28,8 +27,8 @@ barplot(deaths_ordered$Value,names.arg = deaths_ordered$Country, las=2, col=rain
 #Barplot Deaths in 2015
 subset_2015 <- subset(data_infant, data_infant["Year"]==2015)
 deaths_2015 <- aggregate(Value ~ Country, data = subset_2015, FUN = sum)
-deaths_ordered <- deaths_2015[order(deaths_2013$Value),]
-barplot(deaths_ordered$Value,names.arg = deaths_ordered$Country, las=2, col=rainbow(length(deaths_ordered$Value)),cex.names = 0.5,main="Barplot Deaths in 2013")
+deaths_ordered <- deaths_2015[order(deaths_2015$Value),]
+barplot(deaths_ordered$Value,names.arg = deaths_ordered$Country, las=2, col=rainbow(length(deaths_ordered$Value)),cex.names = 0.5,main="Barplot Deaths in 2015")
 
 #Barplot Deaths in 2021
 subset_2021 <- subset(data_infant, data_infant["Year"]==2021)
@@ -46,6 +45,5 @@ barplot(deaths_ordered$Value,names.arg = deaths_ordered$Country, las=2, col=rain
 #Barplot esempio ( visualizzare le morti della colombia per anno (divisi per masure che sono 3 ))
 subset_Colombia <- subset(data_infant, data_infant["Country"]== "Colombia" & data_infant$Measure == "Deaths per 1 000 live births")
 deaths_Colombia <- aggregate(Value ~ Year, data = subset_Colombia, FUN = sum)
-deaths_ordered <- deaths_Colombia[order(deaths_Colombia$Value),]
+#deaths_ordered <- deaths_Colombia[order(deaths_Colombia$Value),]
 barplot(deaths_Colombia$Value,names.arg = deaths_Colombia$Year, las=2, col=rainbow(length(deaths_ordered$Value)),cex.names = 0.5,main="Barplot Deaths in Colombia")
-
